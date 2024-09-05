86757fb4d8c23ae658987fb73cbd65f0463f9ea8
Ravindraas <145589020+Ravindraas@users.noreply.github.com>
2024-09-05 23:44:25 +0530

    Add files via upload

---
+++ responsive-nav-menu/script.js
@@ -0,0 +1,11 @@
+// Select the navbar
+const navbar = document.getElementById("navbar");
+
+// Add event listener for scrolling
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
